24b1e90cb4311e3ef2bd73399d806ecadada7ced
kyhorne <me@kyhorne.com>
2019-02-21 16:41:59 -0500

    Updated pgrm1.asm

--- assembly/pgrm1.asm
+++ assembly/pgrm1.asm
@@ -2,7 +2,7 @@
 ; Search an array to see if it contains a known value.
 
 ; Assumptions:
-; R4 initially contains the start address of the array;
+; R4 initially contains the start address of the array.
 ; R5 initially contains the number of elements in the array.
 ; R3 initially contains the “value of interest”.
 ; If the “value of interest” is in the array, then at the end of execution, R6
@@ -22,14 +22,14 @@
 ; }
 
             mvn r6, #0       ; r6 = -1
-            mov r7, #0       ; initialize loop r7 = 0
-            b   TestForDone  ; test for done at end of loop!
-DoFor       ldr r8, [r4, r7] ; get element array[r7]
-            cmp r8, r3       ; element == value of interest
-            bne IncR7        ; No  - continue loop
-            mov r6, r7       ; Yes - save index
-            b   DoneFor      ;     - break
+            mov r7, #0       ; Initialize loop r7 = 0.
+            b   TestForDone  ; Test for done at end of loop!
+DoFor       ldr r8, [r4, r7] ; Get element array[r7]
+            cmp r8, r3       ; Element == value of interest.
+            bne IncR7        ; No  - Continue loop.
+            mov r6, r7       ; Yes - Save index.
+            b   DoneFor      ;     - Break.
 IncR7       add r7, r7, #1   ; r7++
 TestForDone cmp r7, r5       ; r7 < r5
-            blt DoFor        ; Yes - dop loop body again
-            DoneFor          ; continue
+            blt DoFor        ; Yes - Do loop body again.
+            DoneFor          ; Continue.
